af9fa0402dca0b32752eba81beba5c249fb22098
Pulkit077 <pulkitagarwal2k1@gmail.com>
2023-12-11 10:29:03 +0530

    drivers: rc: import IR changes from sweet_k6a
     - from sweet_k6a-r-oss kernel source
    
    Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

--- drivers/media/rc/ir-spi.c
+++ drivers/media/rc/ir-spi.c
@@ -158,7 +158,7 @@ static int ir_spi_remove(struct spi_device *spi)
 }
 
 static const struct of_device_id ir_spi_of_match[] = {
-	{ .compatible = "ir-spi-led" },
+	{ .compatible = "qcom,spi-msm-ir" },
 	{},
 };
 MODULE_DEVICE_TABLE(of, ir_spi_of_match);
